3912e356099fb3761d254c1b095af3dc9e166c52
iven germonpre <iven.germonpre@gmail.com>
2021-12-02 11:09:06 +0100

    init

---
+++ src/main/java/Case.java
@@ -0,0 +1,4 @@
+public class Case {
+     private static String TEAMID = "6a2a6ada-303c-40c7-a87a-9b9370030e8f";
+     
+}
--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,25 +1,8 @@
 
+public class main {
 
-
-
-public class Main {
-
-    URL url = new URL("https://htf.bewire.org/");
-    HttpURLConnection con = (HttpURLConnection) url.openConnection();
-    private static String GET = "GET";
-    private static String POST = "POST";
-    private static String TEAMID = "6a2a6ada-303c-40c7-a87a-9b9370030e8f";
-
-
-    public static void main(String args[]){
-
-
-    }
-
-
-    public Object sendRequest(String method, String body){
-        con.setRequestMethod(method);
-
+    public static void main(String[] args) {
+        System.out.println("hello");
     }
 }
 
